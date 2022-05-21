944f7f1d81e83cf99b1d67491fdaba710273ee82
KarolPilarski <74739669+KarolPilarski@users.noreply.github.com>
2022-05-21 20:06:02 +0200

    Delete PROJEKT2/GUI_PROJ_2 directory

--- PROJEKT2/GUI_PROJ_2/src/Projekt2/Point.java
+++
@@ -1,9 +0,0 @@
-package Projekt2;
-
-public class Point extends Course{
-    double i;
-    Point(Country start,Country destination,Boolean infected,int currentX,int currentY,double i){
-        super(start,destination,infected,currentX,currentY);
-        this.i=i;
-    }
-}
